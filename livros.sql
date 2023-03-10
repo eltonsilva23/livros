512e038fcf95724e1d92e731ff7ed91201e69235
eltonsilva23 <jose.elton@escolar.ifrn.edu.br>
2023-03-10 12:03:18 -0300

    ãlterando banco

---
+++ livros.sql
@@ -0,0 +1,13 @@
+CREATE TABLE livros(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    ano INTEGER NOT NULL,
+    genero VARCHAR(100) NOT NULL
+);
+
+railway
+containers-us-west-19.railway.app
+fQPC3bZ7QViRJDr5A6bh
+7793
+root
